36a3cebcde67488d70a42c0b708e0fbbae73da86
mayosorio <may.osorio@duocuc.cl>
2024-07-06 13:28:35 -0400

    nosotros
    actualizacion

--- tienda/urls.py
+++ tienda/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('base', views.base, name="base")
+    path('base', views.base, name="base"),
+    path('nosotros',views.nosotros, name="nosotros"),
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 def base(request):
     context = {}
     return render(request, 'base.html',context)
+def nosotros(request):
+    context = {}
+    return render(request, 'nosotros.html',context)
